58f4d47db3bb2a88bd6521031890ccc6f82cbc6f
François <contact@cyannlab.com>
2021-03-15 13:12:11 +0100

    Création du controller show et géstion des vues home et show

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +13,26 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        // return $this->json([
-        //     'message' => 'Welcome to your new controller!',
-        //     'path' => 'src/Controller/HomeController.php',
-        // ]);
+        $articles = $articleRepository->findAll();
 
-        return $this->render("home/home.html.twig");
+        return $this->render("home/home.html.twig", [
+            'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="show_article")
+     */
+    public function show($id, ArticleRepository $articleRepository): Response
+    {
+        $article = $articleRepository->find($id);
+        if (!$article) return $this->redirectToRoute('home');
+
+
+        return $this->render("showArticle/show.html.twig", [
+            'article' => $article,
+        ]);
     }
 }
