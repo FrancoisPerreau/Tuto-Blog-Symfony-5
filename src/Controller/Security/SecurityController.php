a121ef5cbbfdca80c6847fee6102a1ffd72adce2
François <contact@cyannlab.com>
2021-03-16 23:20:11 +0100

    feat: Encodage password et creation Action login dans SecurityController

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -23,24 +24,38 @@ class SecurityController extends AbstractController
     /**
      * @Route("/inscription", name="security_register")
      */
-    public function register(Request $request): Response
+    public function register(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
         $user = new User;
         $form = $this->createForm(RegisterType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            // Encode le mot de passe avant l'enregistrement de l'utilisateur en bdd
+            $passwordHash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($passwordHash);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
             $this->addFlash('success', 'Votre compte a été créé avec succès');
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('security_login');
         }
 
         return $this->render('security/register.html.twig', [
             'pageTitle' => 'Inscription',
             'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig', [
+            'pageTitle' => 'Connexion',
         ]);
     }
 }
